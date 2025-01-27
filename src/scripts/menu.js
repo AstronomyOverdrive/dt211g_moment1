7f947b370fce3f19ba6ddce06597d987060eb6b8
William Pettersson <wipe2400@student.miun.se>
2025-01-27 21:14:46 +0100

    HTML template + new build script

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Get elements
+const menuButton = document.getElementById("menu-btn");
+const navMenu = document.getElementById("dropdown");
+
+// Current menu state
+let menuOpen = false;
+
+function toggleMenu(){
+    menuOpen = !menuOpen; // Toggle bool
+    if (menuOpen){ // Set elements display state
+        navMenu.style.display = "block";
+    } else {
+        navMenu.style.display = "none";
+    }
+}
+
+// Event listener
+menuButton.addEventListener("click", toggleMenu);
